0f2d596048c7a6240bb7a8ae38d08a7dc5dc741b
book_swag <its.bookswag@gmail.com>
2020-11-09 23:54:48 +0900

    Parse sample.txt and Print each line - https://www.geeksforgeeks.org/how-to-read-a-file-line-by-line-to-string-in-golang/

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("HOLA")
-	log.Fatalf("Oh, no!")
+
+	file, err := os.Open("sample.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+	var text []string
+
+	for scanner.Scan() {
+		text = append(text, scanner.Text())
+	}
+	file.Close()
+
+	for _, each_ln := range text {
+		fmt.Println(each_ln)
+		fmt.Println("HOLA")
+	}
+
 }
